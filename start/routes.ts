caba661010ae51ab242ed26011d824fa911b2fd1
ducyt2509 <duclee025@gmail.com>
2024-07-25 13:53:33 +0700

    Initial project and docker setup

---
+++ start/routes.ts
@@ -0,0 +1,13 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.on('/').render('pages/home')
+router.get('/bean' , (ctx) => ctx.response.send('Hello Bean'))
